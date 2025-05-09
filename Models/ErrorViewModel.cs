f72b6d1f229fc4f86eb038ae057a4b680d5ec4d1
Trần Tuấn Thịnh <tran.tuan.thinh.0125@gmail.com>
2025-05-09 23:24:22 +0700

    reset git

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TranTuanThinh_2031200036_Lab.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
